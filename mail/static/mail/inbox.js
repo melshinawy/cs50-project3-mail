aad463b4275e1e417bc020330af6c3d35406fe6d
melshinawy <melshinawy@users.noreply.github.com>
2023-05-29 20:36:00 +0300

    Implemented email reply, archive and email not sent error

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -11,25 +11,36 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('input[type="submit"]').onclick = send;
 
   // Add a placeholder in the recipients field to instruct the user on how to send to multiple recipients
-  document.querySelector('#compose-recipients').placeholder = 'Separate multiple recipients with commas';
-
-  // Set an action to the send button when clicked
-  
+  document.querySelector('#compose-recipients').placeholder = 'Separate multiple recipients with commas';  
 
   // By default, load the inbox
   load_mailbox('inbox');
 });
 
-function compose_email() {
+function compose_email(email=null) {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
+  if (email.subject === undefined) {
+    to = '';
+    subject = '';
+    body = '';
+
+  } else {
+    to = email.sender;
+    subject = email.subject.slice(0,3) === 'Re:' ? email.subject : `Re: ${email.subject}`;
+    body = `
+    On ${email.timestamp} ${email.sender} wrote:
+    "${email.body}"
+    `    
+  }
+
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  document.querySelector('#compose-recipients').value = to;
+  document.querySelector('#compose-subject').value = subject;
+  document.querySelector('#compose-body').value = body;
 }
 
 function load_mailbox(mailbox) {
@@ -62,27 +73,23 @@ function load_mailbox(mailbox) {
     // Create a row for each email containing the information in emailItems
     emails.forEach(email => {
       let row = mailboxTable.insertRow();
+
+      // Change mouse cursor and text format when hovering over an email
       row.style.cursor = 'pointer';
       row.addEventListener('mouseover', () => {
         row.style.color = 'blue';
         row.style.textDecoration = 'underline'
       })
 
+      // Change back text format when mouse is not over an email
       row.addEventListener('mouseout', () => {
         row.style.color = 'black';
         row.style.textDecoration = 'none'
       })
 
-      row.addEventListener('click', () => {
-        document.querySelector('#emails-view').innerHTML = `
-          <div>
-            <p><span style="font-weight: bold;">From: </span>${email.sender}<br>
-            <span style="font-weight: bold;">To: </span>${email.recipients}<br>
-            <span style="font-weight: bold;">Subject: </span>${email.subject}<br>
-            <span style="font-weight: bold;">Timestamp: </span>${email.timestamp}<br>
-            <a href="#" class="btn btn-primary">Reply</a></p>
-        </div>
-        `
+      // When an email is clicked toggle viewEmail view
+      row.addEventListener('click', function () {
+        viewEmail(email, mailbox);
       });
 
       // Have read emails with a grey background and unread emails in white background
@@ -107,8 +114,8 @@ function load_mailbox(mailbox) {
 
 
 // Function for when the Send button is clicked
-function send(event) {
-  event.preventDefault()
+function send() {
+  // event.preventDefault()
   // Create objects for recipients, subject and email body
   subject = document.querySelector('#compose-subject');
   body = document.querySelector('#compose-body');
@@ -125,18 +132,72 @@ function send(event) {
   })
   .then(response => response.json())
   .then(result => {
-    // TODO: Redirect the user to an error page if the email is not sent
-    if (result.message === 'Email sent successfully.') {
+    console.log(result)
+    // Load the sent mailbox if the email is sent successfully, else show an error message
+    if (result.message === undefined) {
+      document.querySelector('#emails-view').innerHTML = `
+      <h1>Error:</h1>
+      <p>Did not send email. ${result.error}</p>
+      `
+      document.querySelector('#emails-view').style.display = 'block';
+      document.querySelector('#compose-view').style.display = 'none';
+    } else {
       load_mailbox('sent');
+             
     }
-    console.log(result);
+  }).catch(error => {
+    
   });
 }
 
-// Function for when an emai is clicked
+// Function for when an email is clicked
 
-function viewEmail(email) {
-  document.querySelector('#emails-view').innerHTML = '';
-  console.log(email);
+function viewEmail(email, mailbox) {
 
+  // Mark email as read
+  if (!email.read){
+    fetch(`/emails/${email.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          read: true
+      })
+    })  
+  }
+
+  // Generate the Archive button text based on whether or not the email is marked as archived
+  const archiveText = email.archived ? 'Move to inbox' : 'Archive'
+
+  // Create a view for email message
+  document.querySelector('#emails-view').innerHTML = `
+    <div style="border-bottom: solid lightgrey 1px; margin-bottom: 10px; padding-bottom: 10px;">
+      <p><span style="font-weight: bold;">From: </span>${email.sender}<br>
+      <span style="font-weight: bold;">To: </span>${email.recipients}<br>
+      <span style="font-weight: bold;">Subject: </span>${email.subject}<br>
+      <span style="font-weight: bold;">Timestamp: </span>${email.timestamp}<br>
+      </p>
+      <button id="reply" class="btn btn-sm btn-outline-primary">Reply</button>
+      <button id="archive" class="btn btn-sm btn-outline-primary">${archiveText}</button>
+    </div>
+  <div>${email.body}</div>
+  `;
+
+  // Action for when the Reply button is clicked
+  document.querySelector('#reply').onclick = function () {
+    compose_email(email);
+  }
+
+  if (mailbox === 'sent') {
+    document.querySelector('#archive').style.visibility = 'hidden';
+  }
+  
+  // Action for when the archive button is clicked
+  document.querySelector('#archive').onclick = function () {
+    fetch(`/emails/${email.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          archived: !email.archived
+      })
+    })
+    load_mailbox('archive')
+  }
 }
